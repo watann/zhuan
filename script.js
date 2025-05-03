dec43c2d97d3949d6ea68c334062f7a74d224813
Watan Hama <157245802+watann@users.noreply.github.com>
2025-05-03 14:54:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function showFlavors() {
 function closeModal() {
     document.getElementById('flavorModal').style.display = 'none';
 }
+function showFlavor() {
+    document.getElementById('flavorModa').style.display = 'block';
+}
+
+function closeModa() {
+    document.getElementById('flavorModa').style.display = 'none';
+}
 
 window.onclick = function(event) {
     if (event.target == document.getElementById('flavorModal')) {
